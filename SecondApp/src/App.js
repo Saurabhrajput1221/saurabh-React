958a0288856aa05c6946b4e4a18bf871b7bbd118
saurabh rajput <saurabhkgf2609@gmail.com>
2022-03-07 23:05:10 +0530

    A

---
+++ SecondApp/src/App.js
@@ -0,0 +1,18 @@
+// import React from "./Componets/BestReact/BestReact";
+import MyReact from "./Comp/MyReact";
+
+// import Surprise from './Componets/Tours/Surprise';
+import './App.css'
+
+function App() {
+  // let expenseDate = new Date(2022, 2, 28);
+  // let expenseTitle = "Dinner";
+  // let expenseAmount = 300;
+  return (
+    <div>
+    <MyReact></MyReact>
+    </div>
+  );
+}
+
+export default App;
---
+++ SecondApp/src/Comp/MyReact.js
@@ -0,0 +1,8 @@
+
+function MyReact(){
+    return(
+        <>
+      <h1> This is My highest level of React;</h1>    </>
+    )
+}
+export default MyReact;
