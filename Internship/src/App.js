004119e88ce1f424a76b1fba2f302f1b6c9c010b
saurabh rajput <saurabhkgf2609@gmail.com>
2022-03-09 23:15:47 +0530

    A

---
+++ Internship/src/App.js
@@ -0,0 +1,16 @@
+
+import One from './Component/One'
+import './App.css'
+import Two from './Component/Two';
+
+function App() {
+ 
+  return (
+    <div>
+  <One></One>
+  <Two></Two>
+    </div>
+  );
+}
+
+export default App;
---
+++ Internship/src/Component/Two.js
@@ -0,0 +1,8 @@
+function Two(){
+    return(
+        <div>
+            <h1>Two</h1>
+        </div>
+    )
+}
+export default Two;
